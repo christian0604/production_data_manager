fce1c1692c91f3832dfe04b35558947c828aafff
Simplex <Simplex@VM-WIN10PROX64>
2017-10-23 08:55:57 -0400

    initial commit

---
+++ Simplex_PDM/src/main/java/com/simplexsys/simplex_pdm/SimplexPdmApplication.java
@@ -0,0 +1,12 @@
+package com.simplexsys.simplex_pdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplexPdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplexPdmApplication.class, args);
+	}
+}
